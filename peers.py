8de94b0631dfed46656fe18023f895d470b89c70
Michael Skalka <michael.skalka@canonical.com>
2018-11-28 18:17:42 -0500

    cleaned up to match state of built charm

--- peers.py
+++ peers.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python
 
-from charms.reactive import (
-    hook,
-    RelationBase,
-)
-from charms.reactive.bus import (
-    StateList,
-    State,
-)
+from charms.reactive import RelationBase
+from charms.reactive import hook
 from charms.reactive import scopes
 
 
 class LxdCluster(RelationBase):
-    scope = scopes.SERVICE
 
-    class states(StateList):
-        connected = State('{relation_name}.connected')
-        joined = State('{relation_name}.joined')
-        departed = State('{relation_name}.departed')
+    scope = scopes.UNIT
 
-    @hook('{peers:lxd-cluster}-relation-{joined,changed}')
-    def peer_joined_or_changed(self):
-        self.set_state('{relation_name}.connected')
-        self.set_trigger_like_state(self.states.joined)
+    @hook('{peers:lxd-cluster}-relation-joined')
+    def peers_joined(self):
+        conv = self.conversation()
+        conv.set_state('{relation_name}.joined')
 
     @hook('{peers:lxd-cluster}-relation-departed')
-    def peer_departed(self):
-        self.set_trigger_like_state(self.states.departed)
-        if not self.units():
-            self.remove_state('{relation_name}.connected')
+    def peers_departed(self):
+        conv = self.conversation()
+        conv.remove_state('{relation_name}.joined')
+        conv.set_state('{relation_name}.departing')
